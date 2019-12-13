57579c0630c91f17d93389435fe613c8ea1efd91
rutuls <rutulshah2007@gmail.com>
2019-12-14 01:23:19 +0530

    Added Post page content
    Post title,description,Edit and Save buttons shown in post.html after clicking on elipsis from postlist.html

--- js/postslist.js
+++ js/postslist.js
@@ -15,6 +15,18 @@ function closeDeleteModal() {
     modal.style.display = 'none';
 }
 
+function openPost(id) {
+    var title = document.getElementById(`title-${id}`).childNodes;
+    title = title[1].innerHTML;
+    var description = document.getElementById(`desc-${id}`).innerHTML;
+    var author = document.getElementById(`post-author-${id}`).childNodes;
+    author = author[1].innerHTML;
+    localStorage.setItem('title', title);
+    localStorage.setItem('description', description);
+    localStorage.setItem('author', author);
+    window.location.href='post.html';
+}
+
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
